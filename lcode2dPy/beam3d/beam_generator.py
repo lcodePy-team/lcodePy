e7123c0816346cb223cb446cd8f9530e5a451193
gonalexander <gornalexander@gmail.com>
2021-03-15 14:10:14 +0700

    Config merging and diagnostics 2d class

--- lcode2dPy/beam3d/beam_generator.py
+++ lcode2dPy/beam3d/beam_generator.py
@@ -28,19 +28,8 @@ def rGauss(sigma=1, vmin=0, vmax=1):
     rgauss_maker.sigma = sigma
     return rgauss_maker
 
-class Simulation():
-    def __init__(self, geometry='circ', xi_step=0.01, x_step=0.01, y_step=0.01, xi_size=1, 
-                 x_size=5, y_size=5, beam_partic_in_layer=200):
-        self.geometry=geometry
-        self.xi_step = xi_step
-        self.x_step  = x_step
-        self.y_step  = y_step
-        self.xi_size = xi_size
-        self.x_size  = x_size
-        self.y_size  = y_size
-        
-def make_beam(self, xi_distr, pz_distr, Ipeak_kA,
-              x_distr=None, y_distr=None, r_distr=None, px_distr=None, py_distr=None, ang_distr=None, 
+def make_beam(config, xi_distr, pz_distr, Ipeak_kA,
+              x_distr=None, y_distr=None, r_distr=None, px_distr=None, py_distr=None, ang_distr=None,
               q_m=1.0, partic_in_layer=200,
               savehead=False, saveto=False, name='beamfile.bin'):
         if q_m == 1 and Ipeak_kA > 0:
@@ -49,34 +38,34 @@ def make_beam(self, xi_distr, pz_distr, Ipeak_kA,
 
         if xi_distr.med > 0:
             print('Warning: Beam center is in xi>0.')
-            
-        # xi_step = self.xi_step
-    
+
+        # xi_step = config.getfloat('xi_step')
+
         if saveto and 'beamfile.bin' in os.listdir(saveto):
             raise Exception('Another beamfile.bin is found. You may delete it using the following command: "!rm %s".' % os.path.join(saveto, name))
         I0 = 17.03331478052319 # kA
         q = 2.*Ipeak_kA/I0/partic_in_layer
-        gamma = pz_distr.med 
-        N = partic_in_layer / self.xi_step / xi_distr.f0
+        gamma = pz_distr.med
+        N = partic_in_layer / config.getfloat('xi-step') / xi_distr.f0
         N = int(round(N))
         xi = xi_distr(N)
         if savehead:
-            xi = xi[xi >= -self.xi_size]
+            xi = xi[xi >= -config.getfloat('window-length')]
             N = len(xi)
         else:
-            xi = xi[(xi >= -self.xi_size) & (xi <= 0)]
+            xi = xi[(xi >= -config.getfloat('window-length')) & (xi <= 0)]
             N = len(xi)
-        partic_in_mid_layer = np.sum((xi > xi_distr.med - self.xi_step/2) & (xi < xi_distr.med + self.xi_step/2))
+        partic_in_mid_layer = np.sum((xi > xi_distr.med - config.getfloat('xi-step')/2) & (xi < xi_distr.med + config.getfloat('xi-step')/2))
         print('Number of particles:', N)
         print('Number of particles in the middle layer:', partic_in_mid_layer)
         xi = np.sort(xi)[::-1]
 
-        if self.geometry == '3d':
+        if config.get('geometry') == '3d':
             x = x_distr(N)
             if y_distr is not None:
                 y = y_distr(N)
             else:
-                y = x_distr(N)  
+                y = x_distr(N)
             np.random.shuffle(x)
             np.random.shuffle(y)
             pz = pz_distr(N)
@@ -93,43 +82,43 @@ def make_beam(self, xi_distr, pz_distr, Ipeak_kA,
             beam_data = np.vstack([particles.T, stub_particle])
             beam = df(beam_data, columns=['xi', 'x', 'y', 'pz', 'px', 'py', 'q_m', 'q', 'N'])
         head = beam[beam.eval('xi>0')]
-        beam = beam[beam.eval('xi<=0'.format(-self.xi_size))]
+        beam = beam[beam.eval('xi<=0'.format(-config.getfloat('window-length')))]
         if saveto:
             beam.values.tofile(os.path.join(saveto, name))
         if savehead:
             head.values.tofile(os.path.join(saveto, 'head-' + name))
         return beam
 
-def main():
-    geometry = '3d'
-    Simulation.make_beam = make_beam
-    sim = Simulation(geometry=geometry)
-
-    angspread = 1e-5
-    m_proton = 958/0.51
-    Ipeak_kA = 40/1000
-    gamma = 426 * 5 * m_proton
-
-    xi_distr=Gauss(vmin=-10, vmax=0)
-    x_distr=Gauss(vmin=-7.5, vmax=7.5)
-    px_distr=Gauss(sigma=gamma*angspread, vmin=None, vmax=None)
-    pz_distr=Gauss(gamma, gamma*1e-4, vmin=None, vmax=None)
-
-    beam = sim.make_beam(xi_distr=xi_distr, x_distr=x_distr, px_distr=px_distr,
-                         pz_distr=pz_distr, Ipeak_kA=Ipeak_kA, q_m=1/m_proton,
-                         partic_in_layer=1000)
-
-    np.savez_compressed('beamfile',
-                        xi = beam['xi'].to_numpy(),
-                        x = beam['x'].to_numpy(),
-                        y = beam['y'].to_numpy(),
-                        p_x = beam['px'].to_numpy(),
-                        p_y = beam['py'].to_numpy(),
-                        p_z = beam['pz'].to_numpy(),
-                        q_m = beam['q_m'].to_numpy(),
-                        q_norm = beam['q'].to_numpy(),
-                        id = beam['N'].to_numpy('int'))
-
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     geometry = '3d'
+#     Simulation.make_beam = make_beam
+#     sim = Simulation(geometry=geometry)
+#
+#     angspread = 1e-5
+#     m_proton = 958/0.51
+#     Ipeak_kA = 40/1000
+#     gamma = 426 * 5 * m_proton
+#
+#     xi_distr=Gauss(vmin=-10, vmax=0)
+#     x_distr=Gauss(vmin=-7.5, vmax=7.5)
+#     px_distr=Gauss(sigma=gamma*angspread, vmin=None, vmax=None)
+#     pz_distr=Gauss(gamma, gamma*1e-4, vmin=None, vmax=None)
+#
+#     beam = sim.make_beam(xi_distr=xi_distr, x_distr=x_distr, px_distr=px_distr,
+#                          pz_distr=pz_distr, Ipeak_kA=Ipeak_kA, q_m=1/m_proton,
+#                          partic_in_layer=1000)
+#
+#     np.savez_compressed('beamfile',
+#                         xi = beam['xi'].to_numpy(),
+#                         x = beam['x'].to_numpy(),
+#                         y = beam['y'].to_numpy(),
+#                         p_x = beam['px'].to_numpy(),
+#                         p_y = beam['py'].to_numpy(),
+#                         p_z = beam['pz'].to_numpy(),
+#                         q_m = beam['q_m'].to_numpy(),
+#                         q_norm = beam['q'].to_numpy(),
+#                         id = beam['N'].to_numpy('int'))
+#
+#
+# if __name__ == '__main__':
+#     main()
