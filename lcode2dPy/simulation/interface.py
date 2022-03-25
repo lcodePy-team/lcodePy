95554eb6945ff78f135773b1b27fb1d2009fbac4
gornalexander <gornalexander@gmail.com>
2022-03-25 15:21:06 +0700

    New diagnostics

--- lcode2dPy/simulation/interface.py
+++ lcode2dPy/simulation/interface.py
@@ -11,10 +11,6 @@ from lcode2dPy.beam.beam_io import MemoryBeamSource, MemoryBeamDrain
 from lcode2dPy.plasma.initialization import init_plasma
 from lcode2dPy.diagnostics.targets import MyDiagnostics
 
-# Imports for 3d simulation
-from lcode2dPy.push_solver_3d import PushAndSolver3d
-from lcode2dPy.beam3d.beam import particle_dtype as beam_particle_dtype_3d
-
 
 class Simulation:
     def __init__(self, config=default_config, beam_generator=make_beam,
@@ -23,10 +19,7 @@ class Simulation:
         self.t_step = config.getfloat('time-step')
 
         geometry = config.get('geometry')
-        if  geometry == '3d' or geometry == '3D':
-            self.push_solver = PushAndSolver3d(self.config) # 3d
-            self.beam_particle_dtype = beam_particle_dtype_3d
-        elif geometry == 'circ' or geometry == 'c':
+        if geometry == 'circ' or geometry == 'c':
             self.push_solver = PusherAndSolver(self.config) # circ
             self.beam_particle_dtype = beam_particle_dtype_2d
         elif geometry == 'plane':
@@ -40,14 +33,13 @@ class Simulation:
         self.beam_source = None
         self.beam_drain = None
         
-        self.diagnostics = MyDiagnostics(config, diagnostics)
+        self.diagnostics = diagnostics
 
     def step(self, N_steps):
         """Compute N time steps."""
         # t step function, makes N_steps time steps.
 
         # Beam generation
-        self.diagnostics.config()
         if self.beam_source is None:
             beam_particles = self.beam_generator(self.config, **self.beam_pars)
             beam_particles = np.array(list(map(tuple, beam_particles.to_numpy())),
@@ -56,8 +48,8 @@ class Simulation:
             beam_slice = BeamSlice(beam_particles.size, beam_particles)
             self.beam_source = MemoryBeamSource(beam_slice) #TODO mpi_beam_source
             self.beam_drain = MemoryBeamDrain()
-        if self.diagnostics:
-            self.diagnostics.config = self.config
+#         if self.diagnostics:
+#             self.diagnostics.sim = self
         # Time loop
         for t_i in range(N_steps):
             fields, plasma_particles = init_plasma(self.config)
@@ -76,17 +68,17 @@ class Simulation:
             #     self.diagnostics.every_dt()
             
 
-class Diagnostics2d:
-    def __init__(self, dt_diag, dxi_diag):
-        self.config = None
-        self.dt_diag = dt_diag
-        self.dxi_diag = dxi_diag
+# class Diagnostics2d:
+#     def __init__(self, dt_diag, dxi_diag):
+#         self.config = None
+#         self.dt_diag = dt_diag
+#         self.dxi_diag = dxi_diag
 
-    def every_dt(self):
-        pass # TODO
+#     def every_dt(self):
+#         pass # TODO
 
-    def every_dxi(self, t, layer_idx, plasma_particles, plasma_fields, rho_beam, beam_slice):
-        for diag_name in self.dxi_diag.keys():
-            diag, pars = self.dxi_diag[diag_name]
-            diag(self, t, layer_idx, plasma_particles, plasma_fields, rho_beam, beam_slice, **pars)
-        return None
+#     def every_dxi(self, t, layer_idx, plasma_particles, plasma_fields, rho_beam, beam_slice):
+#         for diag_name in self.dxi_diag.keys():
+#             diag, pars = self.dxi_diag[diag_name]
+#             diag(self, t, layer_idx, plasma_particles, plasma_fields, rho_beam, beam_slice, **pars)
+#         return None
