11f3512ce741d058ccb7879d7a2ccb87b5fd1b8b
Ivan Kargapolov <i.kargapolov@g.nsu.ru>
2022-11-02 16:59:26 +0700

    plasma3d_gpu: combine dual and single plasma approaches together

--- lcode2dPy/plasma3d_gpu/rhoj.py
+++ lcode2dPy/plasma3d_gpu/rhoj.py
@@ -1,44 +1,30 @@
 """Routine to compute charge density and currents of particles."""
 from ..config.config import Config
 from .data import GPUArrays
-from .weights import deposit as deposit_single
-from .weights_dual import deposit as deposit_dual
+from .weights import get_deposit_func
 
-class RhoJComputer_dual(object):
+class RhoJComputer(object):
     def __init__(self, config: Config):
         self.grid_step_size = config.getfloat('window-width-step-size')
         self.grid_steps = config.getint('window-width-steps')
-        self.plasma_coarseness = config.getint('plasma-coarseness')
         self.plasma_fineness = config.getint('plasma-fineness')
+        
+        dual_plasma_approach = config.getbool('dual-plasma-approach')
+        if dual_plasma_approach:
+            self.plasma_coarseness = config.getint('plasma-coarseness')
+        else:
+            self.plasma_coarseness = None
+        
+        self.deposit_func = get_deposit_func(
+            dual_plasma_approach, self.grid_steps, self.grid_step_size,
+            self.plasma_coarseness, self.plasma_fineness)
 
     def compute_rhoj(self, particles: GPUArrays, const_arrays: GPUArrays):
-        ro, jx, jy, jz = deposit_dual(
-            self.grid_steps, self.grid_step_size, 
-            self.plasma_coarseness, self.plasma_fineness,
-            particles.x_offt, particles.y_offt,
-            particles.px, particles.py, particles.pz,
-            particles.q, particles.m, const_arrays
-        )
-
-        # Also add the background ion charge density.
-        # Do it last to preserve more float precision
-        ro += const_arrays.ro_initial
-
-        return GPUArrays(ro=ro, jx=jx, jy=jy, jz=jz)
-
-
-class RhoJComputer_single(object):
-    def __init__(self, config: Config):
-        self.grid_step_size = config.getfloat('window-width-step-size')
-        self.grid_steps = config.getint('window-width-steps')
-
-    def compute_rhoj(self, particles: GPUArrays, const_arrays: GPUArrays):
-        ro, jx, jy, jz = deposit_single(
-            self.grid_steps, self.grid_step_size,
+        ro, jx, jy, jz = self.deposit_func(
             particles.x_init, particles.y_init,
             particles.x_offt, particles.y_offt,
             particles.px, particles.py, particles.pz,
-            particles.q, particles.m
+            particles.q, particles.m, const_arrays
         )
 
         # Also add the background ion charge density.
