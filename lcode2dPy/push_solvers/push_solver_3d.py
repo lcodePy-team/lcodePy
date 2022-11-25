68a658ed0d60e79711784ebf1a2916e1d9e576cc
Ivan Kargapolov <i.kargapolov@g.nsu.ru>
2022-11-25 18:05:38 +0700

    3D: change 3D diagnostics classes, 3D beam generator (alt_beam_generator), and 3D simulation class to prepare them for merging with 2D code

--- lcode2dPy/push_solvers/push_solver_3d.py
+++ lcode2dPy/push_solvers/push_solver_3d.py
@@ -83,5 +83,7 @@ class PushAndSolver3d:
                 f'xi={-xi_i * self.xi_step_size:+.4f} Ez={Ez_00:+.4e}'
             )
 
-        if diagnostics:
-            diagnostics.dump(current_time)
+        # Perform diagnostics
+        if diagnostics:       
+            diagnostics.dump(current_time, pl_particles, pl_fields,
+                             pl_currents, beam_drain)
