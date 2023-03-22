05b533d51b2bb7b167caa512cbef4264b66b4bba
Ivan Kargapolov <i.kargapolov@g.nsu.ru>
2023-03-22 15:09:36 +0700

    plasma2d: got rid of nested functions for plasma particles deposition

--- lcode2dPy/plasma/rhoj.py
+++ lcode2dPy/plasma/rhoj.py
@@ -1,44 +1,87 @@
 """Routine to compute currents of particles."""
 import numpy as np
-from numba import njit
+import numba as nb
+
+from math import sqrt, floor
 
 from ..config.config import Config
 from .data import Arrays
-from .gamma import gamma_mass_nb
-from .weights import (
-    deposit_antisymmetric,
-    deposit_symmetric,
-    particles_weights,
-)
 
 
-@njit
-def _deposit_currents(r_step, n_cells, r, p_r, p_f, p_z, q, m):
-    grid_particle_params = particles_weights(r, r_step, n_cells)
-    gamma_mass = gamma_mass_nb(m, p_r, p_f, p_z)
+@nb.njit
+def weight_quadratic(local_coordinate, place):
+    """
+    Corresponds to the area of the triangular particle in the corresponding cell.
+    """
+    # TODO: Change to switch statement (match and case) when Python 3.10 is
+    #       supported by Anaconda.
+    if place == -1:
+        return (local_coordinate - 1 / 2) ** 2 / 2
+    if place == 0:
+        return 3 / 4 - local_coordinate ** 2
+    if place == 1:
+        return (local_coordinate + 1 / 2) ** 2 / 2
 
-    # Particle charge depends on velocity to save continuity equation in QSA
-    deposited_charge = q / (gamma_mass - p_z)
 
-    drho = deposited_charge * gamma_mass
-    dj_r = deposited_charge * p_r
-    dj_f = deposited_charge * p_f
-    dj_z = deposited_charge * p_z
+@nb.njit(parallel=True)
+def deposit_plasma(grid_step_size, grid_steps, r, p_r, p_f, p_z, q, m):
+    """
+    Deposit plasma particles onto the charge density and current grids.
+    """
+    out_rho = np.zeros(grid_steps, dtype=np.float64)
+    out_j_r = np.zeros(grid_steps, dtype=np.float64)
+    out_j_f = np.zeros(grid_steps, dtype=np.float64)
+    out_j_z = np.zeros(grid_steps, dtype=np.float64)
 
-    rho = deposit_symmetric(drho, grid_particle_params)
-    j_z = deposit_symmetric(dj_z, grid_particle_params)
-    j_r = deposit_antisymmetric(dj_r, grid_particle_params)
-    j_f = deposit_antisymmetric(dj_f, grid_particle_params)
+    for k in nb.prange(m.size):
+        gamma_mass = sqrt(m[k] ** 2 + p_r[k] ** 2 + p_f[k] ** 2 + p_z[k] ** 2)
+
+        # Particle charge depends on velocity to save continuity equation in QSA
+        deposited_charge = q[k] / (gamma_mass - p_z[k])
+        drho = deposited_charge * gamma_mass
+        dj_r = deposited_charge * p_r[k]
+        dj_f = deposited_charge * p_f[k]
+        dj_z = deposited_charge * p_z[k]
+
+        r_normalized = r[k] / grid_step_size
+        particle_cell_index = int(floor(r_normalized + 0.5))
+        r_local = r_normalized - particle_cell_index
+
+        first_place = -1
+        # A unique case for particles in the central cell
+        # that are deposited antisymmetrically
+        if particle_cell_index == 0:
+            unique_place = -1; cell_index = 1
+            weight = weight_quadratic(r_local, unique_place)
+
+            out_rho[cell_index] += drho * weight
+            out_j_z[cell_index] += dj_z * weight
+            out_j_r[cell_index] -= dj_r * weight
+            out_j_f[cell_index] -= dj_f * weight
+
+            first_place = 0
+
+        for place in range(first_place, 2):
+            weight = weight_quadratic(r_local, place)
+            cell_index = particle_cell_index + place
+            
+            if particle_cell_index == grid_steps - 1 and place == 1:
+                cell_index = grid_steps - 2 # Why?
+
+            out_rho[cell_index] += drho * weight
+            out_j_z[cell_index] += dj_z * weight
+            out_j_r[cell_index] += dj_r * weight
+            out_j_f[cell_index] += dj_f * weight
 
     # Due to symmetry
-    j_r[0] = 0
-    j_f[0] = 0
+    out_j_r[0] = 0
+    out_j_f[0] = 0
     # Boundary condition
-    j_r[n_cells - 1] = 0
-    j_f[n_cells - 1] = 0
-    j_z[n_cells - 1] = 0
+    out_j_r[grid_steps - 1] = 0
+    out_j_f[grid_steps - 1] = 0
+    out_j_z[grid_steps - 1] = 0
 
-    return rho, j_r, j_f, j_z
+    return out_rho, out_j_r, out_j_f, out_j_z
 
 
 def _cell_volume(r_step, particles_per_cell, n_cells):
@@ -59,8 +102,10 @@ def get_rhoj_computer(config: Config):
     particles_per_cell = config.getint('plasma-particles-per-cell')
     cell_volume = _cell_volume(grid_step_size, particles_per_cell, n_cells)
 
-    def compute_rhoj(particles):
-        rho, j_r, j_f, j_z = _deposit_currents(
+    def compute_rhoj(particles: Arrays):
+        xp = particles.xp
+
+        rho, j_r, j_f, j_z = deposit_plasma(
             grid_step_size, n_cells, particles.r,
             particles.p_r, particles.p_f, particles.p_z,
             particles.q, particles.m)
