1d7f83ed9052c6b4a97a33c74f3942566d5addb4
Ivan Kargapolov <i.kargapolov@g.nsu.ru>
2023-03-28 18:26:23 +0700

    Noise filter: add transverse (perpendicular) noise reduction in addition to longitudinal

--- lcode2dPy/plasma3d/noise_filter.py
+++ lcode2dPy/plasma3d/noise_filter.py
@@ -5,15 +5,20 @@ from ..config.config import Config
 from .data import Arrays
 
 
+# This function generates a noise filtering function which
+# we then use in the predictor-corrector loop (solver.py).
 def get_noise_filter(config: Config):
-    xi_step_size     = config.getfloat('xi-step')
+    # Here it is much more convenient to take the config parameters only once,
+    # and not just give them to the noise_filter along with the particles arrays.
+    xi_step_size = config.getfloat('xi-step')
 
     filter_window_length = config.getint('filter-window-length')
     filter_polyorder = config.getint('filter-polyorder')
     filter_coefficient = config.getfloat('filter-coefficient')
 
+    # A new noise mitigation method.
     def noise_filter(particles: Arrays):
-        # A new noise mitigation method. Firstly, we get all get all required arrays:
+        # First, we make views for all required arrays (similar to numpy.ndarray.view):
         x_init, y_init = particles.x_init, particles.y_init
         x_offt, y_offt = particles.x_offt, particles.y_offt
         px, py, pz = particles.px, particles.py, particles.pz
@@ -21,6 +26,8 @@ def get_noise_filter(config: Config):
         # Particle displacement is (x_offt, y_offt). Displacement inhomogeneity:
         dx_inhom = x_offt[1:-1, :] - (x_offt[2:, :] + x_offt[:-2, :]) / 2
         dy_inhom = y_offt[:, 1:-1] - (y_offt[:, 2:] + y_offt[:, :-2]) / 2
+        dx_inhom_perp = y_offt[1:-1, :] - (y_offt[2:, :] + y_offt[:-2, :]) / 2
+        dy_inhom_perp = x_offt[:, 1:-1] - (x_offt[:, 2:] + x_offt[:, :-2]) / 2
 
         # Sagol filter-averaged displacement inhomogeneity:
         dx_inhom_averaged = savgol_filter(
@@ -29,14 +36,24 @@ def get_noise_filter(config: Config):
         dy_inhom_averaged = savgol_filter(
             x=dy_inhom, window_length=filter_window_length,
             polyorder=filter_polyorder, axis=1)
+        dx_inhom_averaged_perp = savgol_filter(
+            x=dx_inhom_perp, window_length=filter_window_length,
+            polyorder=filter_polyorder, axis=0)
+        dy_inhom_averaged_perp = savgol_filter(
+            x=dy_inhom_perp, window_length=filter_window_length,
+            polyorder=filter_polyorder, axis=1)
 
         # Chaotic displacement:
         dx_chaotic = dx_inhom - dx_inhom_averaged
         dy_chaotic = dy_inhom - dy_inhom_averaged
+        dx_chaotic_perp = dx_inhom_perp - dx_inhom_averaged_perp
+        dy_chaotic_perp = dy_inhom_perp - dy_inhom_averaged_perp
 
         # Restoring force:
         force_x = - filter_coefficient * dx_chaotic
         force_y = - filter_coefficient * dy_chaotic
+        force_x_perp = - filter_coefficient * dx_chaotic_perp
+        force_y_perp = - filter_coefficient * dy_chaotic_perp
 
         # Uncomment this to test a relativism corrected filter:
         # gamma_m = np.sqrt(m**2 + pz**2 + px**2 + py**2)
@@ -50,14 +67,20 @@ def get_noise_filter(config: Config):
         # py[:, 2:]   -= factor_1[2:, :]   * force_y / 2
         # py[:, :-2]  -= factor_1[:-2, :]  * force_y / 2
 
-        # A filter with relativism corrections:
+        # A filter without relativism corrections:
         px[1:-1, :] += xi_step_size * force_x
         px[2:, :]   -= xi_step_size * force_x / 2
         px[:-2, :]  -= xi_step_size * force_x / 2
+        py[1:-1, :] += xi_step_size * force_x_perp
+        py[2:, :]   -= xi_step_size * force_x_perp / 2
+        py[:-2, :]  -= xi_step_size * force_x_perp / 2
 
         py[:, 1:-1] += xi_step_size * force_y
         py[:, 2:]   -= xi_step_size * force_y / 2
         py[:, :-2]  -= xi_step_size * force_y / 2
+        px[:, 1:-1] += xi_step_size * force_y_perp
+        px[:, 2:]   -= xi_step_size * force_y_perp / 2
+        px[:, :-2]  -= xi_step_size * force_y_perp / 2
 
         return Arrays(particles.xp, x_init=x_init, y_init=y_init,
                       x_offt=x_offt, y_offt=y_offt,
