9223cfe8c94241b1206ae9d41b1d4b0d0031c4ad
Petr Tuev <p.v.tuev@inp.nsk.su>
2024-02-20 13:49:31 +0700

    Add Config to lcode import

--- lcode/__init__.py
+++ lcode/__init__.py
@@ -1,10 +1,6 @@
 """Quasistatic code for plasma wakefield simulation."""
 
-from .alt_beam_generator.beam_generator import generate_beam
-# from .simulation import test
-from .simulation import Simulation #as Cartesian3dSimulation
-from .diagnostics.diagnostics_3d import (
-    DiagnosticsFXi, DiagnosticsColormaps, DiagnosticsTransverse, SaveRunState
-)
+from .simulation import Simulation
+from .config import Config
 
 __version__ = "0.1.0"
