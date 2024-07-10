ef9df4e042cfed30bca0eef2238f24dea13b5469
Petr Tuev <p.v.tuev@inp.nsk.su>
2024-07-10 11:42:36 +0700

    Fix rigid beam axis mismathed

--- lcode/beam3d/beam_calculator.py
+++ lcode/beam3d/beam_calculator.py
@@ -157,7 +157,7 @@ class RigidBeamCalculator:
 
         grid = ((self.xp.arange(grid_steps) - grid_steps // 2)
                 * grid_step_size)
-        self.x_grid, self.y_grid = self.xp.meshgrid(grid, grid)
+        self.y_grid, self.x_grid = self.xp.meshgrid(grid, grid)
     
     def start_time_step(self):
         """A dummy function for the rigid-beam mode."""
